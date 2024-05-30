8b942d1a2bf60e078d033b161ceb3768b45465eb
terikaisterika <trililium@yandex.ru>
2024-05-30 09:06:32 +0300

    добавлен вывод первичной валидации

--- src/script.js
+++ src/script.js
@@ -1,12 +1,10 @@
 /**
- * Получаем
- * @param {number[]} arr
- * @param {number} maxLength
- * @returns
+ * Проверка длины массива
+ * @param {number[]} arr массив значений
+ * @param {number} maxLength максимальная длина массива
+ * @returns возврат '' или строка с ошибкой
  */
 function checkLength(arr, maxLength = 7) {
-  console.log(arr);
-  console.log(arr.length);
   let messageMax = '';
   let messageMin = '';
   if (arr.length > maxLength) {
@@ -17,6 +15,13 @@ function checkLength(arr, maxLength = 7) {
   }
   return `${messageMax}${messageMin}`;
 }
+/**
+ * Проверка: все числа в пределах минимальных, максимальных чисел
+ * @param {number[]} arr массив значений
+ * @param {number} minNumber минимально возможное число
+ * @param {number} maxNumber максимально возможное число
+ * @returns
+ */
 function checkMaxMin(arr, minNumber = -10, maxNumber = 10) {
   messageMax = '';
   messageMin = '';
@@ -31,58 +36,88 @@ function checkMaxMin(arr, minNumber = -10, maxNumber = 10) {
 
   return `${messageMax}${messageMin}`;
 }
+/**
+ * @param {number[]} arr массив значений
+ * @returns строка. Ели все ок: значение(индекс)
+ * если есть ошибки на длину, макс, минимум, то строка с пояснением.
+ */
 function getMax(arr) {
-  //let messageLength = '';
   messageLength = checkLength(arr);
-
   messageMaxMin = checkMaxMin(arr);
-
   if (messageLength != '' || messageMaxMin != '') {
     return `Ошибка в данных: ${messageLength}${messageMaxMin}`;
   }
   extremeValue = arr[0];
   ind = 0;
   for (let i = 1; i < arr.length; i++) {
-    let el = Number(arr[i]);
-    if (extremeValue < el) {
-      extremeValue = el;
+    if (extremeValue < arr[i]) {
+      extremeValue = arr[i];
       ind = i;
     }
   }
   return `${extremeValue}(${ind})`;
 }
+/**
+ * Получаем веб элемент по локатору
+ * @param {string} locator локатор, по которому можно идентифицировать
+ * элемент на странице. Ищет первый элемент.
+ * @returns найденный элемент
+ */
 function getWebElement(locator) {
   return document.querySelector(locator);
 }
+/**
+ * срабатывает при буквах, точках, цифры больше 2
+ */
 function checkInput() {
   regExp = /^-?\d{1,2}(,{1}-?\d{1,2})*$/g;
   if (regExp.test(this.value)) {
     return;
   } else {
+    outputAnswer(this.value, '#errorInput');
+    hideElement(containerErrorInput, false);
     this.value = 0;
   }
 }
+/**
+ *
+ * @param {string} result то что собираемся выводить пользователю (значения вычислений, ошибки)
+ * @param {string} locator локатор элемента, в который вставится ответ.
+ */
 function outputAnswer(result, locator) {
-  divContainerAnswer = getWebElement('#containerAnswer');
-  hideElement(divContainerAnswer, false);
   divAnswer = getWebElement(locator);
   divResult = document.createElement('div');
   divResult.innerText = result;
   divAnswer.appendChild(divResult);
 }
-
+/**
+ * Получаем массив чисел
+ * @param {*} list
+ * @returns
+ */
 function getArray(list) {
   arr = list.replaceAll(', ', ',').split(',');
+  arrNumbers = [];
   if (arr.length == 1 && arr[0] == '') {
-    arr = [];
+    arrNumbers = [];
+  } else {
+    arr.forEach((el) => {
+      arrNumbers.push(Number(el));
+    });
   }
-  return arr;
+  return arrNumbers;
 }
+/**
+ * Запускаем большинство логики, которое берет начало с клика на кнопку получения данных
+ * @param {event} e событие клика со всеми его атрибутами
+ */
 function getMaxUI(e) {
   e.preventDefault();
   const form = getWebElement('[name="getMax"]');
   const listValues = getArray(form.elements['list'].value);
   let answer = getMax(listValues);
+
+  hideElement(divContainerAnswer, false);
   outputAnswer(answer, '#answer');
 }
 function hideElement(webElement, hide = true) {
@@ -98,12 +133,21 @@ function hideElement(webElement, hide = true) {
 }
 const buttonSubmit = getWebElement('input[type="submit"]');
 const buttonClear = getWebElement('#clearAnswer');
+
 const input = getWebElement('[name="list"]');
 input.addEventListener('change', checkInput);
 buttonSubmit.addEventListener('click', getMaxUI);
 buttonClear.addEventListener('click', () => {
   divAnswer = getWebElement('#answer');
   divAnswer.innerText = '';
-  divContainerAnswer = getWebElement('#containerAnswer');
-  hideElement(divContainerAnswer);
+  //divContainerAnswer = getWebElement('#containerAnswer');
+  divContainerAnswer.classList.toggle('hidden');
+});
+const errorInput = getWebElement('#errorInput');
+const containerErrorInput = getWebElement('#containerErrorInput');
+const clearErrorInput = getWebElement('#clearErrorInput');
+const divContainerAnswer = getWebElement('#containerAnswer');
+clearErrorInput.addEventListener('click', () => {
+  containerErrorInput.classList.toggle('hidden');
+  errorInput.innerText = '';
 });
